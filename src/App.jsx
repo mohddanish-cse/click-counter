132deb15bf346f807bf7e293aaea2d00a04bb04d
Mohd Danish <>
2021-06-05 17:49:45 +0000

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className="App">
       <button onClick={clickFn}>Click here!</button>
-      {newCount}
+       {newCount}
     </div>
   );
 }
